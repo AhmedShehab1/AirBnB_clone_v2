c1be69a6625cc4aa36dfdc5c6e62d956d720a511
AhmedShehab1 <legendbtw2003@gmail.com>
2024-07-31 07:57:43 +0000

    .

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Module for Flask Framework
+"""
 from flask import Flask
 
 app = Flask(__name__)
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Module for Flask Framework
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
